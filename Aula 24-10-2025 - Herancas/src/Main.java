676da3160e661974b7ddd553ed0b6bc0918495f6
EnzoPrevitale <enzogabrielprevitale@gmail.com>
2025-11-07 08:03:18 -0300

    aaa

---
+++ Aula 24-10-2025 - Herancas/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Colaborador colaborador = new Colaborador(92906659, "Enzo", "Aprendiz");
+
+    }
+}
---
+++ Aula 24-10-2025 - Herancas/src/Mensalista.java
@@ -0,0 +1,6 @@
+public class Mensalista extends Colaborador{
+
+    Mensalista(int edv, String nome, String cargo) {
+        super(edv, nome, cargo);
+    }
+}
